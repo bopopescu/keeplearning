3c82d3d8391241a341be24e2f5920864a2b8ae86
leafsummer <wing.sun1987@gmail.com>
2016-03-24 14:02:59 -0400

    update docker nginx 1.9 docker file

--- docker_test/static_web/run_cmd.sh
+++ docker_test/static_web/run_cmd.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --name docker_nginx  -d -p 8080:80 -v /opt/workplace/nginx/conf.d:/etc/nginx/conf.d -v /opt/workplace/nginx/certs:/etc/nginx/certs -v /var/log/nginx:/var/log/nginx -v /opt/workplace/nginx/www/html:/var/www/html leafsummer/nginx
+docker run --name docker_nginx  -d -p 8080:80 -v /opt/workplace/nginx/conf.d:/etc/nginx/conf.d -v /opt/workplace/nginx/certs:/etc/nginx/certs -v /var/log/nginx:/var/log/nginx -v /opt/workplace/nginx/www/html:/var/www/html leafsummer/nginx:newer
